113c3b475f429aecd4c763d10c62895d96811bbb
Snova1054 <istar.dark@gmail.com>
2022-01-15 13:29:19 -0500

    added modals instead of new screen

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import * as React from 'react';
-import {useState, useReducer} from 'react';
-import { Button, View, Text, TextInput, FlatList, StatusBar, SafeAreaView, TouchableOpacity, StyleSheet  } from 'react-native';
+import React, {useState, useReducer, useEffect} from 'react';
+import {
+  SafeAreaView, StyleSheet, View, Text, FlatList, Modal, TouchableOpacity, TextInput, LogoTitle, Button, StatusBar
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import List from './components/List'
+import axios from 'axios';
 
-let writtenEmail;
+
+let DATA;
+
 async function refreshData(){
   DATA = await callingElevatorsAPI();
 }
 async function setEmail(text){
-  writtenEmail = text;
-  await refreshData();
+  DATA = await callingElevatorsAPI();
 }
+
+
 
 const callingElevatorsAPI = async () => {
   try {
@@ -26,90 +30,29 @@ const callingElevatorsAPI = async () => {
   }
 };
 
-
-let DATA;
-
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-    <Text style={[styles.title, textColor]}>Elevator ID#{item.id}{"\n"}Serial #{item.serial_number}</Text>
-  </TouchableOpacity>
-);
-
-const callingEmailAPI = async () => {
-  try {
-    let response = await fetch(
-    `https://hidden-woodland-68127.herokuapp.com/api/users/${writtenEmail}`
-    );
-    let json = await response.json();
-    return json;
-  } catch (error) {
-      console.error(error);
-  }
-};
-let answer;
-const putOperationalAPI = async (id) => {
-  try {
-    let response = await fetch(`https://hidden-woodland-68127.herokuapp.com/api/elevators/${id}/running`, {
-      method: 'PUT',
-      headers: {
-      'Content-Type': 'application/json',
-      },
-      body: "{}",
-  });
-  } catch (error) {
-      console.error(error);
-  }
-  new Promise(resolve => {
-    setTimeout(() => resolve(temporaryName(id)), 1000);
-  });
-  new Promise(resolve => {
-    setTimeout(() => resolve(refreshData()), 1100);
-  });
-  setTimeout(() => {
-    console.log(DATA[0].status)
-  }, 3000);
-  // let response = await temporaryName(id)
-  // setTimeout(() => {
-  
-  // }, 3000);
+function temporaryName(id){
+  fetch(`https://hidden-woodland-68127.herokuapp.com/api/elevators/1`)
+  .then(response => console.log(response.json()))
+  .catch(error=>console.log(error)) //to catch the errors if any
 }
-
-const temporaryName = async(id) => {
-    try {
-    let response = await fetch(
-    `https://hidden-woodland-68127.herokuapp.com/api/elevators/${id}`
-    );
-    let json = await response.json();
-    answer = await json.status;
-    return json;
-  } catch (error) {
-      console.error(error);
-  }
-}
-
-//setTimeout(() => {
-// yourFunction();
-// }, 3000);
-
+// const temporaryName = async(id) => {
+//     try {
+//     let response = await fetch(
+//     `https://hidden-woodland-68127.herokuapp.com/api/elevators/${id}`
+//     );
+//     let json = await response.json();
+//     return await json.status;
+//   } catch (error) {
+//       console.error(error);
+//   }
+// }
 
 function LoginScreen({ navigation }) {
 //Wake up the API
   fetch(`https://hidden-woodland-68127.herokuapp.com/api/users`);
 
   const onLoginPress = async () => {
-    let helloPeople = await callingEmailAPI();
-    if(helloPeople)
-    {
-      navigation.navigate('Home');
-      // DATA = await callingElevatorsAPI();
-    }
-    else
-    {
-      navigation.navigate('Login');
-      alert("Invalid Email");
-    }
-    // helloPeople == true ? navigation.navigate('Home') && (DATA = await callingElevatorsAPI()) && console.log("DATA") : navigation.navigate('Login') && alert("Invalid Email");
+    navigation.navigate('Home');
   }
 
 
@@ -143,116 +86,97 @@ function LoginScreen({ navigation }) {
   );
 }
 
-function useForceUpdate(){
-    const [value, setValue] = useState(0); // integer state
-    return () => setValue(value => value + 1); // update the state to force render
-}
 
 function HomeScreen({ navigation }) {
-  const testingUpdate = useForceUpdate();
-  const [updateView, setUpdateView] = useState(0)
-  const forceUpdate =() => {
-    // if(dataLength != DATA.length)
-    // {
-      setUpdateView((updateView) => ++updateView);
-    // }
-  }
-  // const [, forceUpdate] = useReducer(x => x + 1, 0);
-  // let refreshing = false;
-  // function refreshPage( refreshingOrNot) {
-  //   refreshing = refreshingOrNot
-  //   if(refreshing == true)
-  //   {
-  //     forceUpdate();
-  //     refreshing = false;
-  //   }
-  // }
-  const [selectedId, setSelectedId] = useState(null);
-  
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#d81b3d";
-    const color = item.id === selectedId ? 'white' : 'black';
-    // const navigate = item.id === selectedId ? navigation.navigate("Elevator", { item }) : navigation.navigate("Home");
+  const[data, setData] = useState(DATA)
+  const[isRender, setisRender] = useState(false)
+  const[isModalVisible, setisModalVisible] = useState(false)
+  const[inputText, setinputText] = useState()
+  const[editItem, seteditItem] = useState()
 
-    return (
-      <Item
-        item={item}
-        onPress={() => onPressItem(item)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
-  };
-
+//Pressing an Item on the FlatList, Opens Modal
   const onPressItem = (item) => {
-    if(item != null)
-    {
-      navigation.navigate("Elevator", {id: item.id, entity_type: item.entity_type, serial_number: item.serial_number, status: item.status});
-    }
-  };
-  
-  return (
-    <SafeAreaView  style={{backgroundColor: '#7d8085', flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: StatusBar.currentHeight || 0}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Click refresh"
-        onPress={() => testingUpdate()}
-      />
-      <Button
-        title="Go to Elevator"
-        onPress={() => {
-          /* 1. Navigate to the Elevators route with params */
-          navigation.navigate('Elevator', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          });
-        }}
-      />
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={DATA}
-      />
-
-    </SafeAreaView >
-  );
-}
-
-function ElevatorScreen({ navigation, route }) {
-
-  
-  return (
-    <SafeAreaView  style={{backgroundColor: '#7d8085', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Elevators Screen</Text>
-      <Text style={styles.title}>Elevator #{route.params.id}</Text>
-      <Text style={styles.title}>Serial number #{route.params.serial_number}</Text>
-      <Text style={styles.title}>Entity Type: {route.params.entity_type}</Text>
-      <Text style={styles.title}>Status: {route.params.status}</Text>
-      <Button
-        title="Activate Elevator"
-        onPress={() => putOperationalAPI(route.params.id)}
-        color="#d81b3d"
+    setisModalVisible(true);
+    setinputText(item.status)
+    seteditItem(item.id)
+  }
+//Renders each Item on the FlatList
+  const renderItem = ({item, index}) => {
+    return(
+      <TouchableOpacity
+      style={styles.item}
+      onPress={() => onPressItem(item)}
       >
-      </Button>
-      <Button
-        title="Go to Elevator... again(open another one)"
-        onPress={() =>
-          navigation.push("Elevator", {id: route.params.id, entity_type: route.params.entity_type, serial_number: route.params.serial_number, status: route.params.status})
-        }
+        <Text style={styles.text}>Elevator #{item.id}{"\n"}Status: {item.status}{"\n"}Building type: {item.entity_type}{"\n"}Serial #{item.serial_number}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+//Changes de Data of the FlatList
+  const handleEditItem = (editItem) => {
+    const newData = data.map(item => {
+      if(item.id == editItem){
+        item.status = inputText;
+        return item
+      }
+      return item;
+    })
+    setData(newData);
+    setisRender(!isRender)
+  }
+//Writing on InputText in Modal
+  const hello = async(text) => {
+    setinputText(text)
+  }
+//Pressing Save Button
+  const onPressSaveEdit = async() => {
+    handleEditItem(editItem)
+    let response = await fetch(
+    `https://hidden-woodland-68127.herokuapp.com/api/elevators/${editItem}` //Text in the InputText
+    );
+    let json = await response.json();
+    alert(`you changed the elevator from ${json.status} to ${inputText}`)
+    setTimeout(() => {
+      setisModalVisible(false)
+    }, 3000);
+  }
+
+  return(
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        extraData={isRender}
       />
-      <Button
-        title="Go to Elevator... again(refresh)"
-        onPress={() => navigation.navigate("Elevator", {id: route.params.id, entity_type: route.params.entity_type, serial_number: route.params.serial_number, status: route.params.status})}
-      />
-      <Button title="Go to Home" onPress={() => navigation.push('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
-    </SafeAreaView >
-  );
+      <Modal
+        animationType='fade'
+        visible={isModalVisible}
+        onRequestClose={() => setisModalVisible(false)}
+        >
+          <View style={styles.modalView}>
+            <Text style={styles.text}>
+              Change Status:
+            </Text>
+            <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => hello(text)}
+            defaultValue={inputText}
+            editable={true}
+            multiline={false}
+            maxLength={200}
+            >
+            </TextInput>
+            <TouchableOpacity
+            onPress={() => onPressSaveEdit()}
+            style={styles.touchableSave}
+            >
+            <Text style={styles.text}>Save</Text>
+            </TouchableOpacity>
+          </View>
+      </Modal>
+    </SafeAreaView>
+  )
 }
 
 const Stack = createNativeStackNavigator();
@@ -289,36 +213,46 @@ function App() {
             },
           }}
         />
-        <Stack.Screen
-          name="Elevator"
-          component={ElevatorScreen}
-          options={{
-            title: 'Elevator',
-            headerStyle: {
-              backgroundColor: '#d81b3d',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}
-        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  },
   item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    alignItems: 'flex-start',
+    backgroundColor: '#d81b3d'
   },
-  title: {
-    fontSize: 32,
-    alignItems: "center",
-    justifyContent: "center"
+  text: {
+    marginVertical: 30,
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginLeft: 10
   },
+  textInput: {
+    width: '90%',
+    height: '70',
+    borderColor: 'grey',
+    borderWidth: 1,
+    fontSize: 25,
+  },
+  modalView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  touchableSave: {
+    backgroundColor: 'orange',
+    paddingHorizontal: 100,
+    alignItems: 'center',
+    marginTop: 20,
+  }
 });
 
 export default App;
